12b904f24b971b07dae6baa897891b7a9fc6db61
Luis-Suarezzz <luissuarezhuz@gmail.com>
2023-12-27 23:16:05 -0400

    Aporte de Luis

--- routes/estudiantes.routes.js
+++ routes/estudiantes.routes.js
@@ -37,6 +37,43 @@ router.get('/:id', function (req, res, next) {
     })
 });
 
+/* GET Estudiante por Carrera (Nuevo Endpoint) */
+router.get('/carrera/:carrera', function (req, res, next) {
+  const { carrera } = req.params
+
+  Estudiantes.mostrarCarrera(carrera)
+    .then((resultado) => {
+      res.status(200).json({ "status": 200, "data": resultado })
+    })
+    .catch((error) => {
+      res.status(400).json({ status: 400, mensaje: "no se ha podido mostrar los Estudiantes", error: error })
+    })
+});
+
+/* GET Estudiante por Rango de Edad (Nuevo Endpoint) */
+router.get('/edad/:rangomin/:rangomax', function (req, res, next) {
+  const { rangomin, rangomax } = req.params
+
+  Estudiantes.mostrarRango(Number(rangomin), Number(rangomax))
+    .then((resultado) => {
+      res.status(200).json({ "status": 200, "data": resultado })
+    })
+    .catch((error) => {
+      res.status(400).json({ status: 400, mensaje: "no se ha podido mostrar los Estudiantes", error: error })
+    })
+});
+
+/* GET ultimos 5 estudiantes registrados (Nuevo Endpoint) */
+router.get('/ultimos/5', function (req, res, next) {
+  Estudiantes.mostrarUltimos()
+    .then((resultado) => {
+      res.status(200).json({ "status": 200, "data": resultado })
+    })
+    .catch((error) => {
+      res.status(400).json({ status: 400, mensaje: "no se ha podido mostrar los Estudiantes", error: error })
+    })
+});
+
 /* PUT Estudiantes */
 router.put('/:id', function (req, res, next) {
   const { id } = req.params
